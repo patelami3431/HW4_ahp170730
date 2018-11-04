c302795e8fba04bc3cc18ddcd39f65a5ff5018f8
Ami H Patel <patelami3431@gmail.com>
2018-11-04 16:51:00 -0600

    changed script1.sh

--- HW4/myprog2.c
+++ HW4/myprog2.c
@@ -7,20 +7,22 @@
 #include <unistd.h>
 main()
 {
-	int fd1,fd2,fd3;
+	FILE *fd1;
+	FILE *fd2;
+	FILE *fd3;
 	if(mkdir("dir1", 0777) == -1)
 	{
 		printf("Couldn't create the directory dir1! Try again..");
 		exit(1);
 	}
 	chdir("dir1");
-	fd1 = creat("file10", 0644);
-	if (fd1 < 0)
+	fd1 = fopen("file10", "w");
+	if (fd1 == NULL)
 	{
 		perror("Error in creating file10 in dir1");
 		exit(1);
 	}
-	close(fd1);
+	fclose(fd1);
 	chdir("..");
 	if(mkdir("dir2", 0777) == -1)
 	{
@@ -28,21 +30,21 @@ main()
 	       exit(1);
 	}
 	chdir("dir2");
-	fd2 = creat("file20", 0644);
-	if (fd2 < 0)
+	fd2 = fopen("file20", "w");
+	if (fd2 == NULL)
 	{
 	       perror("Error in creating file20 in dir2");
 	       exit(1);
 	}
-	close(fd2);
+	fclose(fd2);
 	chdir("..");
-	fd3 = creat("file1", 0644);
-	if (fd3 < 0)
+	fd3 = fopen("file1", "w");
+	if (fd3 == NULL)
 	{
 		perror("Error in creating file1");
 		exit(1);
 	}
-	close(fd3);
+	fclose(fd3);
 	char s[100];
 	printf("Creating a soft link in directory: %s\n",getcwd(s,100));
 	if ((symlink("/dir2/file20","link1")) == -1)
--- HW4/script1.sh
+++ HW4/script1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+`mkdir dir1`
+`touch ./dir1/file10`
+`mkdir dir2`
+`touch ./dir2/file20`
+`touch file1`
+`ln -s ./dir2/file20 link1`
+echo "Done!"
 
