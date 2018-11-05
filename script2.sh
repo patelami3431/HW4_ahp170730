c947f8e2cb04f6a01a33359e997af195ac692f86
Ami H Patel <patelami3431@gmail.com>
2018-11-04 18:13:41 -0600

    Final commit

--- script2.sh
+++ script2.sh
@@ -2,9 +2,9 @@
 echo "The current files and folders in the directory are:"
 echo `ls`
 echo "Invoking script3.sh.."
-`cat $HOME/HW4_ahp170730/HW4/script3.sh`
-`chmod +x $HOME/HW4_ahp170730/HW4/script3.sh`
-echo `$HOME/HW4_ahp170730/HW4/script3.sh`
+echo `cat $HOME/HW4_ahp170730/script3.sh`
+`chmod +x $HOME/HW4_ahp170730/script3.sh`
+echo `$HOME/HW4_ahp170730/script3.sh`
 echo "The files and folders in the current directory are:"
 echo `ls`
 
